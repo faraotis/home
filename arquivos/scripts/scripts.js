abf6f6ad0dab1c50f85996ec237da7710727e097
Faraotis <137304409+faraotis@users.noreply.github.com>
2023-12-21 20:10:26 -0300

    fiz um menu responsivo

--- arquivos/scripts/scripts.js
+++ arquivos/scripts/scripts.js
@@ -1,1 +1,16 @@
-// Ainda vou fazer o menu e outras funcionalidades do site
+let menuHam = document.querySelector('.menuHamb')
+let menuClos = document.querySelector('.menuClose')
+let menu = document.getElementsByTagName('menu')[0]
+let nav = document.getElementsByTagName('nav')[0]
+function menuHamb() {
+    menu.style.display = 'block'
+    menuHam.style.display = 'none'
+    menuClos.style.display = 'block'
+    nav.style.display = 'none'
+}
+function menuClose() {
+    menu.style.display = 'none'
+    menuHam.style.display = ''
+    menuClos.style.display = 'none'
+    nav.style.display = ''
+}
