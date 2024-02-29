05e09b9bc684f277a8670be29e2e403bd9681a0b
Faraotis <137304409+faraotis@users.noreply.github.com>
2024-02-29 01:38:19 -0300

    adicionei um footer

--- arquivos/scripts.js
+++ arquivos/scripts.js
@@ -1,37 +1,67 @@
+//Cor
+var purple = '#734082'
+var lightPurple = '#341E3B'
+var darkPurple = '#26162a'
+var $purple = '#4e2d58'
+var $lightPurple = '#734082'
+var $darkPurple = '#341E3B'
+
+//Princ
 var Header = document.getElementsByTagName('header')[0]
 var Tema = document.querySelector('#tema')
 var Abbr = document.querySelector('#abbrTema')
 var Sect = document.querySelector('.contentProjects')
 var Items = document.querySelectorAll('#divTextPr')
+var footerAll = document.getElementsByTagName('footer')[0]
+var divBoxSob = document.querySelectorAll('#boxSob')
+var divBoxSub = document.querySelectorAll('#boxSub')
 var cont = 1
 
 function mudarTema() {
     cont++
     if(cont%2 == 0) {
         //header
-        Header.style.backgroundColor = '#1a1a1a'
+        Header.style.backgroundColor = 'black'
         Header.style.color = 'white'
         Tema.style.backgroundColor = 'white'
-        Abbr.style.backgroundImage = 'linear-gradient(to right, white 0%, white 50%, #1a1a1a 50%, #1a1a1a 100%)'
+        Abbr.style.backgroundImage = 'linear-gradient(to right, white 0%, white 50%, black 50%, black 100%)'
 
         //section
-        Sect.style.backgroundColor = '#1a1a1a'
+        Sect.style.backgroundColor = 'black'
         Sect.style.color = 'white'
         for(var contItems = 0; contItems < Items.length; contItems++) {
             Items[contItems].style.color = 'white'
         }
+
+        //footer
+        footerAll.style.backgroundColor = darkPurple
+
+        //Parte em desenvolvimento
+        // for(var contCor = 0; contCor < 2; contCor++) {
+        //     divBoxSob.style.backgroundColor = purple
+        //     divBoxSub.style.backgroundColor = lightPurple
+        // }
     } else {
         //header
         Header.style.backgroundColor = 'white'
         Header.style.color = '#1a1a1a'
-        Tema.style.backgroundColor = '#1a1a1a'
-        Abbr.style.backgroundImage = 'linear-gradient(to right, #1a1a1a 0%, #1a1a1a 50%, white 50%, white 100%)'
+        Tema.style.backgroundColor = 'black'
+        Abbr.style.backgroundImage = 'linear-gradient(to right, black 0%, black 50%, white 50%, white 100%)'
 
         //section
         Sect.style.backgroundColor = 'white'
-        Sect.style.color = '#1a1a1a'
+        Sect.style.color = 'black'
         for(var contItemsS = 0; contItemsS < Items.length; contItemsS++) {
-            Items[contItemsS].style.color = '#1a1a1a'
+            Items[contItemsS].style.color = 'black'
         }
+
+        //footer
+        footerAll.style.backgroundColor = $darkPurple
+
+        //Parte em desenvolvimento
+        // for(var $contCor = 0; $contCor < 2; $contCor++) {
+        //     divBoxSob.style.backgroundColor = $purple
+        //     divBoxSub.style.backgroundColor = $lightPurple
+        // }
     }
 }
